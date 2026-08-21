780233a2dbdc5a09d00e0ef8a99d71a06791a7ae
Nayaz <mjnayaz1@gmail.com>
2026-08-21 21:14:00 +0430

    delate duplicated element added and tested

--- problems/array-explore.cpp
+++ problems/array-explore.cpp
@@ -19,8 +19,9 @@ using namespace std;
 int FindMax(std::vector<int> nums){
   int maximum; 
   maximum = *max_element(nums.begin(), nums.end());
-  return maximum;
-  }
+  return maximum;  
+}
+
 
 int findSecondLargest(vector<int> nums){
   int maximum = FindMax(nums);
@@ -28,6 +29,7 @@ int findSecondLargest(vector<int> nums){
   int secondmax = FindMax(nums);
   return secondmax;
 }
+
 vector <int> moveZeroEnd (vector<int> nums){
   vector <int> A, B;
   for(std::size_t i=0; i < nums.size(); i++){
@@ -40,6 +42,21 @@ vector <int> moveZeroEnd (vector<int> nums){
     A.insert(A.end(), B.begin(), B.end());
   return A;
 }
+
+vector<int> deleteDuplicated(vector<int> nums) {
+    vector<int> temp;
+
+    for (int i : nums) {
+        auto it = find(temp.begin(), temp.end(), i);
+        // find() returns the target if find. if not found, return temp.end() = False
+        if (it == temp.end()) {
+          // if not found push to temp array
+            temp.push_back(i);
+        }
+    }
+    return temp;
+}
+
 int main(){
    std::vector<int> nums = {4, 0, 7, 4, 0, 9, 2, 9, 3, 1};
 
@@ -50,12 +67,20 @@ int main(){
   for(int i : zeroEnd){
     cout <<i;
   }
+
+  cout <<endl;
+  vector<int> A = deleteDuplicated(nums);
+  cout<<"all duplicated elements delated = ";
+  for(size_t i=0; i<A.size(); i++){
+  cout<<" "<< A[i];
+  }
+  cout<<endl;
   return 0;
 }
 
 
 
-/*
+/* For move zeroes to the end 
 - creat array A and B, 
 - for loop: 
   -add to array A if non-zero element (use A.push_back(number) )
@@ -65,3 +90,12 @@ int main(){
 - O(n).
 */ 
 
+/* For delated duplicated elements 
+- Define a new vector A 
+- For loop over elements,
+- if element not in A, append to A
+- else skip
+if (std::find(my_list.begin(), my_list.end(), x) != my_list.end()) {
+    // x is in the list
+}
+*/
